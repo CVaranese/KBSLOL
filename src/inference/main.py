0ee1b36cb512415f54c303aed63d6509a0387246
calebhskim <caleb.hs.kim@gmail.com>
2018-03-15 23:07:02 -0700

    Remove old main

--- src/inference/main.py
+++
@@ -1,8 +0,0 @@
-from global_model import GlobalModel
-
-if __name__ == '__main__':
-   model = GlobalModel()
-
-   test = {'model': 'SLO_COUNTY', 'location': 'San Luis Obispo',
-      'status': 'Foreclosure', 'bedrooms': 3, 'bathrooms': 2, 'size': 1104}
-   model.predict(test)
